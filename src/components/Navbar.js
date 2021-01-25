34602000a4159ec4c9781a54b06a40aa33d823e2
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-01-25 16:16:57 +0100

    Toggle context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ class Navbar extends Component {
         return (
             <ThemeContext.Consumer>{(context) => {
 
-                    const { isLightTheme, light, dark } = context;
-                    const theme = isLightTheme ? light : dark;
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
                 return (
                     <nav style={{ background: theme.ui, color: theme.syntax }}>
                         <h1>Context App</h1>
