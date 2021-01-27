38dc744e9b71505495c27af798b9c5b91c24b61a
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-01-27 15:29:56 +0100

    Added bookcontextprovider as a function

---
+++ contexthooksapp/src/contexts/BookContext.js
@@ -0,0 +1,18 @@
+import {createContext, useState} from 'react';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState([
+        {title: 'Snowpiercer', id: 1},
+        {title: 'book 2', id: 2},
+        {title: 'book 3', id: 3}
+    ])
+    return (  
+        <BookContext.Provider value={{books}}>
+            {props.children}
+        </BookContext.Provider>
+    );
+}
+ 
+export default BookContextProvider;
