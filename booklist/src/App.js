f38524e8a3d1d4692d7308bdb0e3273391c9e1e9
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-01-28 10:18:46 +0100

    Added bookdetails component

---
+++ booklist/src/App.js
@@ -0,0 +1,16 @@
+import BookList from './components/BookList';
+import Navbar from './components/NavBar';
+import BookContextProvider from './context/BookContext';
+
+function App() {
+  return (
+    <div className="App">
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+      </BookContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ booklist/src/components/BookList.js
@@ -0,0 +1,24 @@
+import {useContext} from 'react';
+import { BookContext } from '../context/BookContext';
+import BookDetails  from './BookDetails';
+
+const BookList = () => {
+    const {books } = useContext(BookContext);
+    return books.length ? (  
+        <div className="book-list">
+            <ul>
+                {books.map(book => {
+                    return (
+                        <BookDetails book={book} key={book.id} />
+                    )
+                })}
+            </ul>
+        </div>
+    ) : (
+        <div className="empty">
+            No book to read.
+        </div>
+    )
+}
+ 
+export default BookList;
